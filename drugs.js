1abe9231368def7d578b9651b2bce5bfa870dfaa
MEGUEDMINI Mohamed <m.bbird21@proton.me>
2025-04-20 15:57:44 +0200

    refactor: add drugs.js with all update functions of each drug

---
+++ drugs.js
@@ -0,0 +1,17 @@
+/* This file contains the update functions for each Drug used by the Pharmacy. */
+
+export function updateHerbalTea() {
+	console.log( "Herbal Tea");
+}
+
+export function updateFervex() {
+	console.log( "Fervex !");
+}
+
+export function updateMagicPill() {
+	console.log( "Magic Pill !");
+}
+
+export function updateDefault() {
+	console.log( "Default !");
+}
